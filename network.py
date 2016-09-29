cdc14a862dea710d7e2f22cafc2b41fb6105de1c
Derek <dowewas2@gmail.com>
2016-09-29 16:06:51 -0500

    add ford fulkerson algorithm code with example

---
+++ network.py
@@ -0,0 +1,16 @@
+fn = FlowNetwork()
+
+fn.addVertex('s', True, False)
+fn.addVertex('t', False, True)
+
+map(fn.addVertex, ['a', 'b','c','d'])
+
+fn.addEdge('s', 'a', 4)
+fn.addEdge('a', 'b', 4)
+fn.addEdge('b', 't', 2)
+fn.addEdge('s', 'c', 3)
+fn.addEdge('c', 'd', 6)
+fn.addEdge('d', 't', 6)
+
+#fn.addEdge('b', 'c', 3)
+
